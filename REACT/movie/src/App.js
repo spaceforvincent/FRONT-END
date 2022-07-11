8c2fc22991b4103d9d65c8960c6a0bdd2de6eb76
spaceforvincent <jmr2882@naver.com>
2022-07-12 06:26:55 +0900

    0712

--- REACT/movie/src/App.js
+++ REACT/movie/src/App.js
@@ -1,8 +1,24 @@
 import { useEffect, useState } from "react";
 import Movie from "./components/Movie.js";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-  return null;
+  return (
+    <Router>
+       <Switch>
+         <Route path="/hello">
+           <h1>Hello</h1>
+         </Route>
+         <Route path="/movie">
+           <Detail />
+         </Route>
+         <Route path="/">
+           <Home />
+         </Route>
+       </Switch>
+     </Router>
+  );
 }
 
 export default App;
