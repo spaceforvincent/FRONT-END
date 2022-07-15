a2aad62e4aa1b0efc0737c978e64ec10a402331a
spaceforvincent <jmr2882@naver.com>
2022-07-15 22:03:18 +0900

    0715

---
+++ REACT/my-app/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Home from "./pages/Home";
+import Counter from "./pages/Counter";
+import About from "./pages/About";
+import Input from "./pages/Input";
+import Input2 from "./pages/Input2";
+import List from "./pages/List";
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/counter">Counter</Link> | <Link to="/input">Input</Link> | <Link to="/input2">Input2</Link> | <Link to="/list">List</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/input" element={<Input />} />
+        <Route path="/input2" element={<Input2 />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
