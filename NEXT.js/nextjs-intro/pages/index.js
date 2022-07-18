9a177f51986d560c44e8899b03a2f9ab104112a3
spaceforvincent <jmr2882@naver.com>
2022-07-18 22:13:26 +0900

    0718

--- NEXT.js/nextjs-intro/pages/index.js
+++ NEXT.js/nextjs-intro/pages/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
+import Link from "next/link";
 
 export default function Home() {
   const [movies, setMovies] = useState();
@@ -14,10 +15,14 @@ export default function Home() {
       <Seo title="Home" />
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <Link href={`/movies/${movie.id}`}key={movie.id}>
+        <a>
+        <div className="movie">
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
+        </a>
+        </Link>
       ))}
       <style jsx>{`
         .container {
@@ -25,6 +30,10 @@ export default function Home() {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
---
+++ NEXT.js/nextjs-intro/pages/movies/[id].js
@@ -0,0 +1,6 @@
+import { useRouter } from "next/router"
+
+export default function Detail () {
+    const router = useRouter();
+    return 'hi'
+}
