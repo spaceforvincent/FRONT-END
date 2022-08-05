bee9e040ff252f0554267d19d5b51e8fa4a1680f
SSAFY <jmr2882@naver.com>
2022-08-06 00:21:54 +0900

    0806

--- NEXT/next-portfolio/components/projects/project-item.js
+++ NEXT/next-portfolio/components/projects/project-item.js
@@ -1,6 +1,6 @@
 export default function ProjectItem({data}) {
     return (
-        <div className="p-6 bg-slate-400 rounded-md">
+        <div className="p-6 m-3 bg-slate-400 rounded-md">
             <h1>{data.properties.Name.title[0].plain_text}</h1>
         </div>
     )
