ad526006246b0a2792506f05e4ba71b83d86de43
SSAFY <jmr2882@naver.com>
2022-08-06 10:52:45 +0900

    배포 전

--- NEXT/next-portfolio/next.config.js
+++ NEXT/next-portfolio/next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images:{
+    domains: [
+      'www.notion.so',
+      'images.unsplash.com',
+      's3.us-west-2.amazonaws.com',
+    ]
+  }
 }
 
 module.exports = nextConfig
